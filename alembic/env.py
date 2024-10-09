be6aa64843a26aa824605ae01303a87847d9335a
sdkhili <128524223+sdkhili@users.noreply.github.com>
2024-10-09 13:57:39 +0100

    Update env.py
    from app.database import Base | to set alembic

--- alembic/env.py
+++ alembic/env.py
@@ -4,9 +4,9 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-#from app.database import Base
+from app.database import Base
 from app.config import settings
-from app.models import Base
+#from app.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
